bae2157d970a3dfb077642d7fcd405aeb8971594
AnaelleD <anaelle.dambreville@gmail.com>
2018-01-21 17:07:46 +0100

    modif du js

--- importCsv.js
+++ importCsv.js
@@ -1,13 +1,25 @@
 function importCsv(laRecette){
+	/*window.location.replace("recette.html");*/
+	
     d3.csv("recettes/Paella.csv", function(error, data){
-        var ingredients = []
-        var preparation = []
-		var cuisson = []
+		
+        var ingredients = [];
+        var preparation = [];
+		var cuisson = [];
 		
         data.forEach(function (d){
-            if(d.mois == months){
-                date.push(d.heure_solaire);
-                sap.push(d.SAP_FLOW);
+            if(d.type == "Ingredients"){
+                ingredients.push(d.detail);
+            }
+			if(d.type == "Preparation"){
+                preparation.push(d.detail);
+            }
+			if(d.type == "Cuisson"){
+                cuisson.push(d.detail);
             }
         })
-		})}
+		console.log(ingredients);
+		console.log(preparation);
+		console.log(cuisson);
+		})
+}
